b8f8bc9e26e6433b278c65a6f8b6b4736e47b8d9
YQzQY <yuzuy@qq.com>
2026-03-19 15:31:19 +0800

    tj

--- problems/动态规划/时空碎片收集者.cpp
+++ problems/动态规划/时空碎片收集者.cpp
@@ -23,20 +23,20 @@ void solve() {
         s[i] = s[i - 1] + a[i];
     }
     vector<vector<ll>>dp(n + m + 2, vector<ll>(k + 2, 0));
-    for (ll i = 1;i <= n + m + 1;++i) {
+    for (ll i = 1;i <= n + 1;++i) {
         dp[i][0] = max(dp[i][0], max(dp[i - 1][0], dp[i - 1][1]) + a[i]);
         dp[i][k + 1] = max(dp[i][k + 1], max(dp[i - 1][0], dp[i - 1][1]) + 2 * a[i]);
         for (ll j = 1;j <= k;++j) {
             dp[i][j] = max(dp[i][j], dp[i - 1][j + 1] + a[i] / 2);
         }
-        // for (ll j = 0;j <= k + 1;++j) {
-        //     dp[min(i + m, n + 1)][max(0ll, j - m - 1)]
-        //         = max(dp[min(i + m, n + 1)][max(0ll, j - m - 1)], dp[i - 1][j] + s[min(i + m, n)] - s[i - 1]);
-        // }
-        if (i > m)
-            for (ll j = 0;j <= k + 1;++j) {
-                dp[min(i, n + 1)][j] = max(dp[min(i, n + 1)][j], dp[i - m - 1][min(j + m + 1, k + 1)] + s[i] - s[i - m - 1]);
-            }
+        for (ll j = 0;j <= k + 1;++j) {
+            dp[min(i + m, n + 1)][max(0ll, j - m - 1)]
+                = max(dp[min(i + m, n + 1)][max(0ll, j - m - 1)], dp[i - 1][j] + s[min(i + m, n)] - s[i - 1]);
+        }
+        // if (i > m)
+        //     for (ll j = 0;j <= k + 1;++j) {
+        //         dp[min(i, n + 1)][j] = max(dp[min(i, n + 1)][j], dp[i - m - 1][min(j + m + 1, k + 1)] + s[i] - s[i - m - 1]);
+        //     }
     }
     ll MAX = 0;
     for (ll i = 0;i <= k + 1;++i) {
