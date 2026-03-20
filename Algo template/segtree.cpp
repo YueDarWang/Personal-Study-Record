c0b958a4dc885afbfedec023f791764074770106
YQzQY <yuzuy@qq.com>
2026-03-20 08:38:27 +0800

    提交

---
+++ Algo template/segtree.cpp
@@ -0,0 +1,34 @@
+#pragma GCC optimize(2)
+
+#include<bits/stdc++.h>
+#define ll long long
+#define int long long
+#define INF 0x3f3f3f3f
+#define MOD 1000000007
+#define mian main
+#define retrun return
+#define endl '\n'
+
+using namespace std;
+
+struct node {
+    bool lazy;
+    ll l, r;
+    ll ans;
+};
+
+void solve() {}
+
+#undef int
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    int t;
+    cin >> t;
+    while (t--) {
+        solve();
+        cout << "\n";
+    }
+}
